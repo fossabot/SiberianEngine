93800adc43afa1a4693343eb23d38c803bbbda97
HashimotoYT <hashimoto.stream@gmail.com>
2017-12-04 21:00:32 +0200

    Create a basic window

---
+++ SiberianEngine-core/main.cpp
@@ -0,0 +1,24 @@
+#include "src/graphics/window.h"
+
+int main()
+{
+    using namespace siberian;
+    using namespace graphics;
+
+    Window window("Siberian!", 960, 540);
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+    while (!window.closed())
+    {
+        window.clear();
+        glBegin(GL_QUADS);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.5f,  0.5f);
+        glVertex2f( 0.5f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+        window.update();
+    }
+
+    return 0;
+}
